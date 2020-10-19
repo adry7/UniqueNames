3ed03ddf05cbf1e3c07763f372c85f9eb69504a9
Mustafa Adry <mustafa.adry2025@gmail.com>
2020-10-19 17:17:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueNames
+{
+    class Program
+    {
+
+        public static string[] uniqueNames(string[] names1,string[] names2)
+        {
+
+          
+            // first Solution 
+            HashSet<string> res = new HashSet<string>(names1);
+            res.Union(names2);
+            return res.ToArray<string>();
+
+
+
+            //After searching online 
+            return  names1.Union(names2).ToArray();
+            
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia"};
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+            Console.WriteLine(string.Join(",", uniqueNames(names1, names2)));
+
+
+        }
+    }
+}
